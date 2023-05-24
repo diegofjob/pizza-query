7ad8228ba73be76d487518991fc2ad920d91c5b1
diegofjob <diegofjob>
2023-05-24 23:48:15 +0000

    OLÁ MUNDO DO PRIMEIRO DIA
    aqui fizemos a primeira consulta....

---
+++ dia01/olaMundo.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+select 'OLÁ MUNDO'
+
+-- COMMAND ----------
+
+SELECT * -- selecione todas as colunas
+FROM silver.pizza_query.pedido -- data tabela silver.pizza.query.pedido
